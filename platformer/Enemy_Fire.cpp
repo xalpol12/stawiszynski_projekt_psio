a392fac4c8c0705f1bf5bc35dde9e58649fc3025
Dawid Stawiszyński <dawid.stawiszynski@student.put.poznan.pl>
2021-05-23 17:00:16 +0200

    Added base Enemy class, each enemy now inherits from this class

--- platformer/Enemy_Fire.cpp
+++ platformer/Enemy_Fire.cpp
@@ -22,10 +22,6 @@ void Enemy_Fire::initTexture(sf::Texture* texture_)
 	this->animationClock.restart();
 }
 
-Enemy_Fire::Enemy_Fire()
-{
-}
-
 Enemy_Fire::Enemy_Fire(sf::Texture* texture_, sf::Vector2f spawnLocation)
 {
 	this->initVariables();
@@ -36,38 +32,6 @@ Enemy_Fire::Enemy_Fire(sf::Texture* texture_, sf::Vector2f spawnLocation)
 
 Enemy_Fire::~Enemy_Fire()
 {
-}
-
-const sf::FloatRect Enemy_Fire::getBounds() const
-{
-	return this->shape.getGlobalBounds();
-}
-
-const sf::Vector2f Enemy_Fire::getPosition() const
-{
-	return this->shape.getPosition();
-}
-
-void Enemy_Fire::restartClock()
-{
-	this->animationClock.restart();
-}
-
-void Enemy_Fire::updateMovement()
-{
-	//Normalize vector
-	this->direction = this->playerPos - this->position;
-	float hyp = sqrt(pow(this->direction.x,2.f) + pow(this->direction.y,2.f));
-	this->direction = direction/hyp;
-
-	//TODO - proximity check
-	this->shape.move(speed*direction.x,direction.y*0.5f*speed);
-
-	if (this->direction.x > 0)
-		this->animState = ENEMY_STATE::ENEMY_ANIMATION_STATE::MOVING_LEFT;
-	else
-		this->animState = ENEMY_STATE::ENEMY_ANIMATION_STATE::MOVING_RIGHT;
-	this->position = this->getPosition();
 }
 
 void Enemy_Fire::updateAnimation()
@@ -114,14 +78,3 @@ void Enemy_Fire::updateAnimation()
 		this->restartClock();
 }
 
-void Enemy_Fire::update(sf::Vector2f playerPos_)
-{
-	this->playerPos = playerPos_;
-	this->updateMovement();
-	this->updateAnimation();
-}
-
-void Enemy_Fire::render(sf::RenderTarget& target)
-{
-	target.draw(this->shape);
-}
--- platformer/Enemy_Fire.h
+++ platformer/Enemy_Fire.h
@@ -1,53 +1,19 @@
 #pragma once
-namespace ENEMY_STATE
-{
-	enum ENEMY_ANIMATION_STATE { IDLE = 0, MOVING_LEFT = 1, MOVING_RIGHT = 2 };
-}
+#include "Enemy.h"
 
-class Enemy_Fire
+
+class Enemy_Fire : public Enemy
 {
 private:
-	//Animation and texture variables
-	const sf::Texture* texture;
-	sf::Sprite shape;
-	sf::Vector2f direction;
-	sf::Vector2f playerPos;
-	sf::Vector2f position;
-	sf::IntRect currentFrame;
-	sf::Clock animationClock;
-	float scalingFactor;
-	short animState;
-
-	//Game variables
-	int points;
-	float speed;
-	int damage;
-
-	//Initialization
 	void initVariables();
 	void initTexture(sf::Texture* texture);
-
 public:
 
-
 	//Constructors
-	Enemy_Fire();
 	Enemy_Fire(sf::Texture* texture_, sf::Vector2f spawnLocation);
 	~Enemy_Fire();
 
-	//Accessors
-	const sf::FloatRect getBounds() const;
-	const sf::Vector2f getPosition() const;
-
-	//Functions
-	void restartClock();
-
 	//Update
-	void updateMovement();
 	void updateAnimation();
-	void update(sf::Vector2f playerPos_);
-
-	//Render
-	void render(sf::RenderTarget& target);
 };
 
